70c302294f95fc55095859396562d9f51f12b436
JennyWeij <jennyweij@outlook.com>
2023-02-24 12:58:28 +0100

    flyttade upp Ladda från local storage till useState()

--- src/hooks/useLocalStorageState.ts
+++ src/hooks/useLocalStorageState.ts
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorageState<State>(initialState: State, key: string) {    
-  const [state, setState] = useState(initialState)
+  const [state, setState] = useState(() => {
+    //Ladda från LS
+    const stringState = localStorage.getItem(key);
+    if (!stringState) return initialState;
+    const state = JSON.parse(stringState) as State;
+    return state;
 
-  // 1. Ladda från LS
-    useEffect(() => {
-      const stringState = localStorage.getItem(key);
-      if (!stringState) return;
-      const state = JSON.parse(stringState) as State;
-      setState(state);
-    }, []);
+  })
 
   // 2. Spara till LS
   useEffect(() => {
